9af4ca568eefe4b3f319d20d804d8d9a5353bee9
rapolurenu <159129847+rapolurenu@users.noreply.github.com>
2024-03-11 17:58:19 +0530

    Update score.js

--- score.js
+++ score.js
@@ -1,1 +1,9 @@
 // Iteration 8: Making scoreboard functional
+const playAgainBtn = document.getElementById("play-again-button");
+const scoreBoard = document.getElementById("score-board");
+
+if (playAgainBtn)
+  playAgainBtn.onclick = function () {
+    window.location.href = "./index.html";
+  };
+  if (scoreBoard) scoreBoard.innerText = localStorage.getItem("score");
